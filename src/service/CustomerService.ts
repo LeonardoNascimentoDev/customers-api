32a5ce6593867993d11b9e8dd3ffb9424f4bd426
Leonardo Nascimento <lsncwb@gmail.com>
2022-05-30 21:52:24 -0300

    feat: adjusting putObject s3

--- src/service/CustomerService.ts
+++ src/service/CustomerService.ts
@@ -18,21 +18,19 @@ export default class CustomerService {
         const customer = await this.repository.findCustomer(payload)
 
         if (!customer) {
-            const format = payload.imgProfile.substring(
-                payload.imgProfile.indexOf(':') + 1,
-                payload.imgProfile.indexOf(';base64'),
-            );
             const s3 = new S3();
             const keyName = payload.name + '_' + new Date().getTime();
             const base64result = payload.imgProfile.substr(payload.imgProfile.indexOf(',') + 1);
             const buff = Buffer.from(base64result, 'base64');
+
             const params = {
                 Bucket: process.env.BUCKET_NAME,
                 Body: buff,
-                Key: `updados/${keyName}​​​​​​​​`,
-                ContentType: format,
+                ContentType: 'image/jpeg',
+                Key: `updados/${keyName}​​​​​​​​.jpg`,
             };
             await s3.putObject(params);
+
             const date = new Date(new Date().valueOf() - new Date().getTimezoneOffset() * 60000)
             payload.registerDate = date
             payload.lastModifyDate = date
